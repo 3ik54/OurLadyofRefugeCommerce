e3d6da22e5d6fcb1b2d94dc0e0a85a222e678c49
angle <frenfrieking@gmail.com>
2025-10-25 20:04:15 -0700

    Fix form only in login

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,9 +1,13 @@
 import './Login.css'
+import Authentication from '../src/Auth.tsx';
 
 export default function Login () {
     return (
+        <>
         <div className='login-container'> 
             <h1>Login</h1>
         </div>
+        <Authentication/>
+        </>
     )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar.tsx';
-
 import ImageSlider from "./ImageSlideshow.tsx";
 import Message from "./Message.tsx";
 import Contact from "../pages/Contact.tsx";
 import Login from '../pages/Login.tsx';
 import Sellers from '../pages/Sellers.tsx'
 import { supabase,key } from './supabase_client.ts';
-import Authentication from './Auth.tsx';
+
 
 
 function App() {
@@ -44,7 +43,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/seller" element={<Sellers />} />
       </Routes>
-      <Authentication/>
+      
     </div>
 
   );
